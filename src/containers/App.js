83b7b2f70d4049bc4eed439b745c5cc2aa4b05d1
Ewa.K <ewa_1988@yahoo.com>
2018-07-18 19:21:55 +0200

    Init app

---
+++ src/containers/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title.js';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+
+        this.addTodo = this.addTodo.bind(this)
+        this.removeTodo = this.removeTodo.bind(this)
+    }
+    addTodo(val) {
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({ data });
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({ data: remainder });
+    }
+
+    render() {  
+	    return (
+	        <div className={style.TodoApp}>
+	            <Title title={'Pozostało ' + this.state.data.length + ' zadań do wykonania.'}/>
+	        </div>
+	    );
+	}
+}
+
+export default App;
+
